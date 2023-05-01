27880b0812588aa3dc415daa0c57400689a7c523
Esron <112833876+esronlinhares@users.noreply.github.com>
2023-05-01 20:55:14 -0300

    Update Assassino.c

--- Assassino.c
+++ Assassino.c
@@ -7,47 +7,47 @@ int main() {
   printf("Telefonou para a vítima? ");
   scanf("%f", &r1);
   if (r1==1) {
-      respostas++;
+    respostas++;
   }
 
   printf("Esteve no local do crime? ");
   scanf("%f", &r2);
   if (r2==1) {
-      respostas++;
+    respostas++;
   }
 
   printf("Mora perto da vítima? ");
   scanf("%f", &r3);
   if (r3==1) {
-      respostas++;
+    respostas++;
   }
 
   printf("Devia para a vítima? ");
   scanf("%f", &r4);
   if (r4==1) {
-      respostas++;
+    respostas++;
   }
 
   printf("Já trabalhou com a vítima? ");
   scanf("%f", &r5);
   if (r5 == 1) {
-      respostas++;
+    respostas++;
   }
 
   if (respostas == 5) {
-      printf("Assassino");
+    printf("Assassino");
   }
 
   if (respostas < 5 ) {
-      printf("Cumplice");
+    printf("Cumplice");
   }
 
   if (respostas == 2) {
-      printf("Suspeito");
+    printf("Suspeito");
   }
 
   if (respostas >= 0 && respostas < 2) {
-      printf("Inocente");
+    printf("Inocente");
   }
 
   return 0;
