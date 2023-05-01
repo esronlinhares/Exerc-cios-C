e93bb67372f81d7484f814a0c6ca288a885754ec
Esron <112833876+esronlinhares@users.noreply.github.com>
2023-05-01 20:49:59 -0300

    Create Verificar Número Maior.c

---
+++ Verificar Número Maior.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+  
+  int n1, n2;
+
+  printf("Escreva o primeiro número: ");
+  scanf("%d", &n1);
+  printf("Escreva o segundo número: ");
+  scanf("%d", &n2);
+
+  if (n1 > n2) {
+      printf("O primeiro número é o maior");
+  }
+  else {
+      printf("O segundo número é  maior");
+  }
+
+  return 0;
+}
