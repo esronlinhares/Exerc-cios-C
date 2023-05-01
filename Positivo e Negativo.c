f38d3e4c269008b9f4b24c4f7d1fbeb76e685f6a
Esron <112833876+esronlinhares@users.noreply.github.com>
2023-05-01 20:47:08 -0300

    Create Positivo e Negativo.c

---
+++ Positivo e Negativo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  
+  int x;
+
+  printf("Escreva o o valor: ");
+  scanf("%d", &x);
+
+  if (x >= 0) {
+      printf("O número é positivo");
+  }
+  else {
+      printf("O número é negativo");
+  }
+
+  return 0;
+}
