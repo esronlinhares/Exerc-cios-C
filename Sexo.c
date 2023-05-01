4b8f08f8d3b113c4e30bb363a733f8166774acf1
Esron <112833876+esronlinhares@users.noreply.github.com>
2023-05-01 20:48:47 -0300

    Create Sexo.c

---
+++ Sexo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  
+  char sexo;
+
+  printf("Escreva o sexo: ");
+  scanf("%c", &sexo);
+
+  if (sexo == 'f' || sexo == 'F') {
+      printf("Feminino");
+  }
+  else if(sexo == 'm' || sexo == 'M') {
+      printf("Masculino");
+  }
+  else {
+      printf("Sexo Inválido");
+  }
+
+  return 0;
+}
