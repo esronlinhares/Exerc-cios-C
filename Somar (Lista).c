cdbae2ef3eb2495ec82d3cffa4867715145da4e2
Esron <112833876+esronlinhares@users.noreply.github.com>
2023-05-01 19:28:28 -0300

    Update Somar (Lista).c

--- Somar (Lista).c
+++ Somar (Lista).c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+    
     int lista[] = {12, 10, 23, 25, 40, 31, 8, 4, 13, 41, 23, 5};
     int x, y, soma;
 
@@ -8,9 +9,9 @@ int main() {
     scanf("%d", &x);
     printf("Escolha um slot de 0 A 11: ");
     scanf("%d", &y);
-    
+
     soma = (lista[x] + lista[y]);
-    
+
     printf("A soma dos valores é: %d", soma);
 
     return 0;
