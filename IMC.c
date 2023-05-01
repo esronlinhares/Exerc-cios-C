7335372a1a870581f1984f494a728c23964e1644
Esron <112833876+esronlinhares@users.noreply.github.com>
2023-05-01 19:28:01 -0300

    Update IMC.c

--- IMC.c
+++ IMC.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    float altura, peso, imc;
     
+    float altura, peso, imc;
+
     printf("Escreva o peso da pessoa: ");
     scanf("%f", &peso);
-    
+
     printf("Escreva a altura da pessoa: ");
     scanf("%f", &altura);
-    
+
     imc = peso / (altura * altura);
-    
+
     printf("O IMC da pessoa e: %2.f", imc);
 
     return 0;
