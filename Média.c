3117cfacbc42f13aa35e1ccf6203af785b8a4e9e
Esron <112833876+esronlinhares@users.noreply.github.com>
2023-05-01 20:54:17 -0300

    Update Média.c

--- Média.c
+++ Média.c
@@ -12,16 +12,16 @@ int main() {
   media = (n1 + n2) / 2;
 
   if (media == 10) {
-      printf("O aluno esta aprovado com distinção com média %.2f", media);
+    printf("O aluno esta aprovado com distinção com média %.2f", media);
   }
   else if (media >= 7) {
-      printf("O aluno esta aprovado com media %.2f", media);
+    printf("O aluno esta aprovado com media %.2f", media);
   }
   else if (media >= 5) {
-      printf("O aluno esta em recuperação com media %.2f", media);
+    printf("O aluno esta em recuperação com media %.2f", media);
   }
   else {
-      printf("O aluno esta reprovado com media %.2f", media);
+    printf("O aluno esta reprovado com media %.2f", media);
   }
 
   return 0;
